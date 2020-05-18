f0c8b0e632ab7db530bedc3d86c43d9b9af7430b
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-05-18 12:38:38 +0900

    아주 기초 정도

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import re
 import os
 
 from Modules.help import Help
+from Modules.setting import *
+
+client = discord.Client()
 
 # Discord Client
 @client.event
@@ -15,17 +18,19 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----------------------')
-    game = discord.Game("!설명으로 도움말")
+    game = discord.Game("-설명으로 도움말")
     await client.change_presence(status=discord.Status.online, activity=game)
-
-    loop.create_task(realtime())
 
 
 @client.event
 async def on_message(msg):
+    channel = msg.channel
     # 봇 설명
-    if message.content == "!설명":
-        createdEmbed = help.create_help_embed()
+    if msg.content == "-설명":
+        createdEmbed = Help.create_help_embed()
         await channel.send(embed=createdEmbed)
+    
+    if msg.content.startswith == "-재생":
+        await channel.send("준비중")
 
 client.run(token)
--- modules/help.py
+++ modules/help.py
@@ -3,7 +3,7 @@ import discord
 
 
 class Help:
-    def create_help_embed(self):
+    def create_help_embed():
         embed = discord.Embed(title="Discord 음악봇입니다",description="Rythm 이 몇번씩 터져서 화가나서 만든 봇입니다.", color=0xf7cac9)
         embed.add_field(name = '-재생 [제목] or [링크]', value = '음악을 재생합니다. 만약 재생중이면 예약이 자동으로 됩니다',inline = False)
         embed.add_field(name='-큐', value='현재 재생되고 있는 노래와 예약된 곡들이 저장된 큐를 출력합니다', inline=False)
